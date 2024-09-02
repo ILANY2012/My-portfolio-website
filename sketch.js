cca425b2db340495c8161ea8db68b69c15ff726d
ILANY2012 <127604927+ILANY2012@users.noreply.github.com>
2024-09-02 12:00:48 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function btn1click(){
+    window.open("https://studio.code.org/home")             
+}
+function btn2click(){
+    window.open("https://codepen.io/")
+}
+function btn3click(){
+    window.open("https://appinventor.mit.edu/")
+}
+
+function btn3click(){
+    window.open("https://code.visualstudio.com/")
+}
+
